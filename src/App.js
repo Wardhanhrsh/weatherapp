a25e085dcd58b0eb56f5f5282bee3a46f29096aa
Harshwardhan Choudhary <choudharyharshwardhan231@gmail.com>
2023-12-30 18:25:36 +0530

    weatherapp

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+// import Todo from "./component/todoreact/todo";
+import Temp from "./component/weather/temp";
+
+const App = () => {
+  return (
+    <>
+      <Temp />
+    </>
+  );
+};
+
+export default App;
